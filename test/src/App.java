f92806f213541c8379e8596ee52283eb63442f38
Placeless <mikoprpr@hotmail.com>
2024-01-09 05:06:14 +0900

    move for prog8

--- test/src/App.java
+++ test/src/App.java
@@ -1,5 +1,136 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!"); //真剣に動くコードはここだけ
+         // double型の変数number1を定義し、8.5を代入してください。
+        double number1 = 8.5;
+    
+        // double型の変数number2を定義し、3.4を代入してください。
+        double number2 = 3.4;
+        
+        // number1にnumber2を足した値を出力してください
+        System.out.println(number1+number2);
+        
+        // number1からnumber2を引いた値を出力してください
+        System.out.println(number1-number2);
+
+        // この文章をコメントにしてください
+        System.out.println("こんにちは、Java");
+        // 「こんにちは、Java」と出力してください
+
+        // 数値の17を出力してください
+        System.out.println(17);
+        
+        // 5に3を足した値を出力してください
+        System.out.println(5+3);
+        
+        // 「5 + 3」を文字列として出力してください
+        System.out.println("5 + 3");
+
+        // 12を3で割った値を出力してください
+        System.out.println(12/3);
+        
+        // 3に6を掛けた値を出力してください
+        System.out.println(3*6);
+        
+        // 8を3で割った時の余りを出力してください
+        System.out.println(8%3);
+
+        // "こんにちは"と"世界"を連結して出力してください
+        System.out.println("こんにちは"+"世界");
+        
+        // "38"と"19"を連結して出力してください
+        System.out.println("38"+"19");
+        // 38と19を足して出力してください
+        System.out.println(38+19);
+
+        // int型の変数numberを定義してください
+        int number;
+        
+        // 変数numberに3を代入してください
+        number = 3;
+        
+        // 変数numberを出力してください
+        System.out.println(number);
+
+        // String型の変数nameを定義してください
+        String name;
+        
+        // 変数nameに"Wanko"を代入してください
+        name = "Wanko";
+        
+        // 変数nameを出力してください
+        System.out.println(name);
+
+        int number1 = 3;
+    
+        // int型の変数number2を定義し、7を代入してください
+        int number2 = 7;
+        
+        // number1 * number2を出力してください
+        System.out.println(number1*number2);
+        
+        // 変数textに「プログラミングを勉強しよう」を代入してください
+        String text = "プログラミングを勉強しよう";
+        
+        // 「Progateで」と変数textを連結して出力してください
+        System.out.println("Progateで"+text);
+
+        int number = 11;
+        String text = "Ruby";
+        System.out.println(number);
+        System.out.println(text);
+        
+        // 変数numberを9で上書きしてください
+        number = 9;
+        
+        // 変数numberを出力してください
+        System.out.println(number);
+        
+        // 変数textを「Java」で上書きしてください
+        text = "Java";
+        
+        // 変数textを出力してください
+        System.out.println(text);
+
+        int number = 3;
+        System.out.println(number);
+        
+        // 変数numberの値に7を足して、変数numberを上書きしてください
+        number = number + 7;
+        
+        // 変数numberを出力してください
+        System.out.println(number);
+
+        int number = 8;
+    
+        // 変数numberに7をかけて、変数numberを上書きしてください
+        number *= 7;
+        
+        // 変数numberを出力してください
+        System.out.println(number);
+        
+        // 変数numberの値に1を足して、変数numberを上書きしてください
+        number++;
+        
+        // 変数numberを出力してください
+        System.out.println(number);
+
+        int length = 6;
+        int height = 8;
+        
+        // 変数rectangleAreaに、四角形の面積を代入してください
+        int rectangleArea = length * height;
+        
+        // 変数rectangleAreaを出力してください
+        System.out.println(rectangleArea);
+        
+        // 変数triangleAreaに、三角形の面積を代入してください
+        int triangleArea =rectangleArea/2;
+        
+        // 変数triangleAreaを出力してください
+        System.out.println(triangleArea);
+
+        
+    
     }
 }
