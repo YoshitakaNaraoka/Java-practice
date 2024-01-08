bc58974e1110e69f5b8a0e8217527aac5b7d44c4
Placeless <mikoprpr@hotmail.com>
2024-01-09 05:25:01 +0900

    nomsg

--- test/src/App.java
+++ test/src/App.java
@@ -1,12 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!","こんにちは、Java",17,5+3,"5 + 3",12/3,3*6,8%3,"こんにちは"+"世界","38"+"19",38+19); //真剣に動くコードはここだけ
+        
         double number1 = 8.5; // double型の変数number1を定義し、8.5を代入してください。
         double number2 = 3.4; // double型の変数number2を定義し、3.4を代入してください。
         System.out.println(number1+number2); // number1にnumber2を足した値を出力してください
         System.out.println(number1-number2); // number1からnumber2を引いた値を出力してください
         // この文章をコメントにしてください
-        System.out.println(); // 「こんにちは、Java」と出力してください
         int number; // int型の変数numberを定義してください
         number = 3; // 変数numberに3を代入してください
         System.out.println(number); // 変数numberを出力してください
@@ -38,8 +37,9 @@ public class App {
         int length = 6;
         int height = 8;
         int rectangleArea = length * height; // 変数rectangleAreaに、四角形の面積を代入してください
-        System.out.println(rectangleArea); // 変数rectangleAreaを出力してください
+        System.out.println(); // 変数rectangleAreaを出力してください
         int triangleArea =rectangleArea/2; // 変数triangleAreaに、三角形の面積を代入してください
-        System.out.println(triangleArea); // 変数triangleAreaを出力してください
+        System.out.println(); // 変数triangleAreaを出力してください
+        System.out.println("Hello, World!","こんにちは、Java",17,5+3,"5 + 3",12/3,3*6,8%3,"こんにちは"+"世界","38"+"19",38+19,rectangleArea,triangleArea); //真剣に動くコードはここだけ
     }
 }
