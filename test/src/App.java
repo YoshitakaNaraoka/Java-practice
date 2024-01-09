cdfee6ef1c489d115c48315af852ec2be638f246
Placeless <mikoprpr@hotmail.com>
2024-01-09 10:07:40 +0900

    num

--- test/src/App.java
+++ test/src/App.java
@@ -1,38 +1,39 @@
 public class App {
     public static void main(String[] args) {
-        int number; // int型の変数numberを定義してください
-        number = 3; // 変数numberに3を代入してください
-        System.out.println(number); // 変数numberを出力してください
+        int number1; // int型の変数numberを定義してください
+        number1 = 3; // 変数numberに3を代入してください
+        System.out.println(number1); // 変数numberを出力してください
         String name; // String型の変数nameを定義してください
         name = "Wanko"; // 変数nameに"Wanko"を代入してください
         System.out.println(name); // 変数nameを出力してください
         
-        System.out.println(number1*number2); // number1 * number2を出力してください
+        
         String text = "プログラミングを勉強しよう"; // 変数textに「プログラミングを勉強しよう」を代入してください
         System.out.println("Progateで"+text); // 「Progateで」と変数textを連結して出力してください
-        int number = 11;
+        int number2 = 11;
         String text = "Ruby";
-        System.out.println(number);
+        System.out.println(number2);
         System.out.println(text);
-        number = 9; // 変数numberを9で上書きしてください
-        System.out.println(number); // 変数numberを出力してください
+        number3 = 9; // 変数numberを9で上書きしてください
+        System.out.println(number3); // 変数numberを出力してください
         text = "Java"; // 変数textを「Java」で上書きしてください
         System.out.println(text); // 変数textを出力してください
-        int number = 3;
-        System.out.println(number);
-        number = number + 7; // 変数numberの値に7を足して、変数numberを上書きしてください
-        System.out.println(number); // 変数numberを出力してください
-        int number = 8;
-        number *= 7; // 変数numberに7をかけて、変数numberを上書きしてください
-        System.out.println(number); // 変数numberを出力してください
-        number++; // 変数numberの値に1を足して、変数numberを上書きしてください
-        System.out.println(number); // 変数numberを出力してください
-        int number1 = 3;
-        int number2 = 7; // int型の変数number2を定義し、7を代入してください
-        double number1 = 8.5; // double型の変数number1を定義し、8.5を代入してください。
-        double number2 = 3.4; // double型の変数number2を定義し、3.4を代入してください。
-        System.out.println(number1+number2); // number1にnumber2を足した値を出力してください
-        System.out.println(number1-number2); // number1からnumber2を引いた値を出力してください
+        int number4 = 3;
+        System.out.println(number4);
+        number4 = number4 + 7; // 変数numberの値に7を足して、変数numberを上書きしてください
+        System.out.println(number4); // 変数numberを出力してください
+        int number5 = 8;
+        number5 *= 7; // 変数numberに7をかけて、変数numberを上書きしてください
+        System.out.println(number5); // 変数numberを出力してください
+        number5++; // 変数numberの値に1を足して、変数numberを上書きしてください
+        System.out.println(number5); // 変数numberを出力してください
+        int number6 = 3;
+        int number7 = 7; // int型の変数number2を定義し、7を代入してください
+        double number8 = 8.5; // double型の変数number1を定義し、8.5を代入してください。
+        double number9 = 3.4; // double型の変数number2を定義し、3.4を代入してください。
+        System.out.println(number6+number7); // number1にnumber2を足した値を出力してください
+        System.out.println(number8-number9); // number1からnumber2を引いた値を出力してください
+        System.out.println(number1*number2); // number1 * number2を出力してください
         int length = 6;
         int height = 8;
         int rectangleArea = length * height; // 変数rectangleAreaに、四角形の面積を代入してください
