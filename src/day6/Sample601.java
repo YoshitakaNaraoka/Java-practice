1091658c95149e5595dc419dee099eb8138fac2a
YoshitakaNaraoka <mikoprpr@hotmail.com>
2025-06-01 06:59:23 +0900

    Measurement Class ProcessTime

--- src/day6/Sample601.java
+++ src/day6/Sample601.java
@@ -3,6 +3,9 @@ package day6;
 public class Sample601 {
  
     public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
         SampleClass01 s = new SampleClass01();  //  インスタンスの生成
         s.n = 100;          //  フィールドnに値を代入
         s.s = "Hello";      //  フィールドsに値を代入
@@ -11,5 +14,10 @@ public class Sample601 {
         System.out.println(ans);        //  呼び出し結果の表示
         System.out.println(str);        //  呼び出し結果の表示
         s.showNum();                    //  メソッドshowNum()を呼び出し
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("処理時間：" + (endTime - startTime) + " ms");
+
     }
 }
